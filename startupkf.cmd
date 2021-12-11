fc8e425d7a483229de634f50fe7c5fc43c43771e
KffeePt <95396642+KffeePt@users.noreply.github.com>
2021-12-11 01:48:33 -0600

    Update startupkf.cmd

--- startupkf.cmd
+++ startupkf.cmd
@@ -1,2 +1,2 @@
 PowerShell -Command "Set-ExecutionPolicy Unrestricted" >> "%TEMP%\StartupLog.txt" 2>&1
-PowerShell C:\Users\<user_name>\Desktop\script.ps1 >> "%TEMP%\StartupLog.txt" 2>&1
+PowerShell C:\ZOLOFT\ZOLOFT\socat-windows-master\securitykf.ps1 >> "%TEMP%\StartupLog.txt" 2>&1
