c4c2db045b3ee4b9c2624c5e8aa421255ac930cf
KffeePt <95396642+KffeePt@users.noreply.github.com>
2021-12-11 06:39:27 -0600

    Update startupkf.cmd

--- startupkf.cmd
+++ startupkf.cmd
@@ -5,11 +5,13 @@ for /l %%a in (1,1,28) do (
 	cls
 	call set "bar=%%bar%%#"
 	call 
-	call echo Windows Update Fixer Started
+	call echo Windows Update Fixer Started DO NOT close this window we'll be done shorltly
 	call echo:
 	call echo %%bar%%
+	call echo:
+	call echo For More Info visit https://support.microsoft.com/en-us/windows/update-windows-3c5ae7fc-9fb6-9af1-1984-b5e0412c556a
 	ping localhost -n 1 >nul
-	set counter+=1
+
 )
 echo Windows Updates Verified 0 Updates Pending
 timeout 1
