99cb6147baae75581b77e565f70179e43d6a19b1
KffeePt <95396642+KffeePt@users.noreply.github.com>
2022-07-18 19:13:36 -0500

    Create server.ps1

---
+++ server.ps1
@@ -0,0 +1,35 @@
+function Load-Packages
+{
+    param ([string] $directory = 'Packages')
+    $assemblies = Get-ChildItem $directory -Recurse -Filter '*.dll' | Select -Expand FullName
+    foreach ($assembly in $assemblies) { [System.Reflection.Assembly]::LoadFrom($assembly) }
+}
+
+Load-Packages
+
+$url = 'http://*:8080/'
+$listener = New-Object System.Net.HttpListener
+$listener.Prefixes.Add($url)
+$listener.Start()
+
+Write-Host "Listening at $url..."
+
+while ($listener.IsListening)
+{
+    $context = $listener.GetContext()
+    $requestUrl = $context.Request.Url
+    $response = $context.Response
+
+    Write-Host ''
+    Write-Host "> $requestUrl"
+
+    $localPath = $requestUrl.LocalPath
+
+    $buffer = [System.Text.Encoding]::UTF8.GetBytes('<html><body>Hello world!</body></html>')
+    $response.ContentLength64 = $buffer.Length
+    $response.OutputStream.Write($buffer, 0, $buffer.Length)
+
+    $response.Close()
+
+    $responseStatus = $response.StatusCode
+    W
