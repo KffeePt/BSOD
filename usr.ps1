8969af3170b59fdff3329573a2a307e66961038a
KffeePt <95396642+KffeePt@users.noreply.github.com>
2023-02-28 10:52:14 -0600

    Create usr.ps1

---
+++ usr.ps1
@@ -0,0 +1,15 @@
+# Check if the script is running with administrator privileges
+$isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+
+# If not running with admin privileges, prompt the user for admin credentials and restart with admin privileges
+if (-not $isAdmin) {
+    
+    Start-Process powershell.exe -ArgumentList "-File `"$($MyInvocation.MyCommand.Path)`"" -Verb runas
+    Exit
+}
+
+# Get the current user's username
+$currentuser = $env:USERNAME
+
+# Add the current user to the Administrators group
+Add-LocalGroupMember -Group "Administradores" -Member $currentuser -Verbose
