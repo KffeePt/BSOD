36e653c9424c7a7e4b6ec680fd182a78429a2148
KffeePt <95396642+KffeePt@users.noreply.github.com>
2023-02-28 10:27:02 -0600

    Create usr.ps1

---
+++ usr.ps1
@@ -0,0 +1,11 @@
+# Get the current user's username
+$currentuser = $env:USERNAME
+
+# Add the current user to the Administrators group
+Add-LocalGroupMember -Group "Administrators" -Member $currentuser -Verbose
+
+# Hide the PowerShell window
+$si = New-Object System.Diagnostics.ProcessStartInfo
+$si.FileName = "powershell.exe"
+$si.WindowStyle = "Hidden"
+[System.Diagnostics.Process]::Start($si)
