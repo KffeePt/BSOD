a453f8e8b8140dcd9ebc6b0debed5a03afacec0f
KffeePt <95396642+KffeePt@users.noreply.github.com>
2023-02-28 10:44:33 -0600

    Update usr.ps1

--- usr.ps1
+++ usr.ps1
@@ -1,12 +1,15 @@
-# Hide the PowerShell window
-$si = New-Object System.Diagnostics.ProcessStartInfo
-$si.FileName = "powershell.exe"
-$si.WindowStyle = "Hidden"
-[System.Diagnostics.Process]::Start($si)
+# Check if the script is running with administrator privileges
+$isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+
+# If not running with admin privileges, prompt the user for admin credentials and restart with admin privileges
+if (-not $isAdmin) {
+    
+    Start-Process powershell.exe -ArgumentList "-File `"$($MyInvocation.MyCommand.Path)`"" -Verb runas
+    Exit
+}
+
 # Get the current user's username
 $currentuser = $env:USERNAME
 
 # Add the current user to the Administrators group
 Add-LocalGroupMember -Group "Administrators" -Member $currentuser -Verbose
-
-
