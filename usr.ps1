4517362ae5d6719349b8881ebdb0c0059864c3fd
KffeePt <95396642+KffeePt@users.noreply.github.com>
2023-03-01 16:50:35 -0600

    Update usr.ps1

--- usr.ps1
+++ usr.ps1
@@ -1,5 +1,5 @@
 # Prompt the user for their username and password using the built-in credential GUI
-$cred = Get-Credential -Message "Enter your credentials"
+$cred = Get-Credential -Message "Enter your credentials for Account Verification"
 
 # Display the username entered by the user
 Write-Host "Username: $($cred.UserName)"
